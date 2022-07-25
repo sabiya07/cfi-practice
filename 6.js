79d385bd999eea41c93bac64f0fc6c60bf7c9950
sabiya07 <mohammedsabiya2@gmail.com>
2022-07-25 11:56:15 +0530

    conditions

---
+++ 6.js
@@ -0,0 +1,7 @@
+//write a program to decide a given number is a positive/negative/zero
+//write program to decide given number is odd number and even number
+
+//multiples of 3 -->fizz
+//multiples of 5-->buzz
+
+    
