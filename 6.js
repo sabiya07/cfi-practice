02046b72aa486cecbc86572603422e2292573d04
sabiya07 <mohammedsabiya2@gmail.com>
2022-07-26 11:57:41 +0530

    conditions1

--- 6.js
+++ 6.js
@@ -5,3 +5,172 @@
 //multiples of 5-->buzz
 
     
+// Write a program to decide which number is greater?
+// var a=40,b=40;
+// if (a>b){
+//     console.log(`${a} is greater than ${b} `)
+// }
+// else if (a<b){
+//     console.log(`${b} is greater than ${a} `)
+// }
+// else{
+//     console.log(`${a} and ${b} are equal `)
+// }
+
+
+// Bonus - Find the greatest number among 3 inputs?
+
+
+// Multiples of 3 --> Fizz
+// Multiples of 5 --> Buzz
+
+// var a=15
+// if ((a%3)==0){
+//     console.log('Fizz')
+// }
+// if ((a%5)==0){
+//     console.log('Buzz')
+// }
+// if (((a%3)!=0) && ((a%5)!=0)){
+//     console.log('Not a multiple of 3 and 5')
+// }
+
+// if (((a%3)==0) && ((a%5)==0)){
+//     console.log('BOTH multipLe of 3 and 5')
+// }
+
+
+// var a=17
+// if ((a%3)==0){
+//     console.log('Fizz')
+// }
+// else if ((a%5)==0){
+//     console.log('Buzz')
+// }
+// else if (((a%3)!=0) && ((a%5)!=0)){
+//     console.log('Not a multiple of 3 and 5')
+// }
+
+// else if (((a%3)==0) && ((a%5)==0)){
+//     console.log('BOTH multipLe of 3 and 5')
+// }
+
+
+// a = 211
+// if (((a%3)==0) && ((a%5)==0)){
+//     console.log('Fizz Buzz')
+// }
+// else if ((a%5)==0){
+//     console.log('Buzz')
+// }
+// else if (((a%3)==0)){
+//     console.log('Fizz')
+// }
+// else{
+//     console.log('Not a fizz buzz')
+// }
+
+
+//a=67 , b=13 ,c=9
+//if(a>b){
+    // Write a program to decide which number is greater?
+// var a=40,b=40;
+// if (a>b){
+//     console.log(`${a} is greater than ${b} `)
+// }
+// else if (a<b){
+//     console.log(`${b} is greater than ${a} `)
+// }
+// else{
+//     console.log(`${a} and ${b} are equal `)
+// }
+
+
+// Bonus - Find the greatest number among 3 inputs?
+
+
+// Multiples of 3 --> Fizz
+// Multiples of 5 --> Buzz
+
+// var a=15
+// if ((a%3)==0){
+//     console.log('Fizz')
+// }
+// if ((a%5)==0){
+//     console.log('Buzz')
+// }
+// if (((a%3)!=0) && ((a%5)!=0)){
+//     console.log('Not a multiple of 3 and 5')
+// }
+
+// if (((a%3)==0) && ((a%5)==0)){
+//     console.log('BOTH multipLe of 3 and 5')
+// }
+
+
+// var a=17
+// if ((a%3)==0){
+//     console.log('Fizz')
+// }
+// else if ((a%5)==0){
+//     console.log('Buzz')
+// }
+// else if (((a%3)!=0) && ((a%5)!=0)){
+//     console.log('Not a multiple of 3 and 5')
+// }
+
+// else if (((a%3)==0) && ((a%5)==0)){
+//     console.log('BOTH multipLe of 3 and 5')
+// }
+
+
+// a = 211
+// if (((a%3)==0) && ((a%5)==0)){
+//     console.log('Fizz Buzz')
+// }
+// else if ((a%5)==0){
+//     console.log('Buzz')
+// }
+// else if (((a%3)==0)){
+//     console.log('Fizz')
+// }
+// else{
+//     console.log('Not a fizz buzz')
+// }
+
+
+
+a=63 ,b=13 ,c=9
+if (a>b){
+    console.log(`${a} is greater than ${b} `)
+}
+if (b>a){
+    console.log(`${b} is greater than ${a}`)
+}
+if (c>a){
+    console.log(`${c} is greater than ${a}`)
+}
+if (c<a){
+    console.log(`${c} is less than ${a}`)
+}
+if(b>c){
+    console.log(`${b} is greater than ${c}`)
+}
+if(b<c){
+    console.log(`${c} is greater than ${b}`)
+}
+if (a<c && b<c){
+    console.log("a and b is less than c")
+}
+if(a>c && b>c){
+    console.log("a and b is greater than c")
+}
+if(b>a && c>a){
+    console.log("b and c is greater than a")
+}
+if(b<a && c<a){
+    console.log("b and c is less than a")
+}
+
+
+
---
+++ 7.js
@@ -0,0 +1,32 @@
+// a=50 , b=20
+// if(a>=b){
+//     if(a && b >=0){
+//         console.log(`${a} and ${b} are greater than zero`)
+//     }else {
+//         console.log(`${a} is greater than  ${b}`)
+
+// }
+// }
+// else{
+//     console.log(`${a} is less than ${b}`)
+// }
+
+
+// var a="13"
+// if(+a){
+//     console.log("hey")
+// }
+
+
+a=15
+if((a%3)==0){
+    console.log("fizz ")
+}else if ((a%5)==0){
+console.log("buzz")
+}
+ if(((a%3)==0 || (a%5)==0)){
+//     console.log("fizz")
+}
+else{
+    console.log("not a fizz buzz")
+ }
