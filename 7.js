66b4352a18bfa87b04761cae820b33c513a538cd
sabiya07 <mohammedsabiya2@gmail.com>
2022-07-28 14:12:53 +0530

    whileloop

--- 7.js
+++ 7.js
@@ -1,32 +1,132 @@
-// a=50 , b=20
-// if(a>=b){
-//     if(a && b >=0){
-//         console.log(`${a} and ${b} are greater than zero`)
-//     }else {
-//         console.log(`${a} is greater than  ${b}`)
+// quaadratic roots and its nature of roots
 
+//  var a=1, b=2,c=4
+//  D=b*b-4*a*c
+//  if (D>0){
+//     console.log((-b)+Math.sqrt(D)/2*a)
+//     console.log((-b)-Math.sqrt(D)/2*a)
+//     console.log('Roots are real')
+//  }
+//     if (D<0){
+//         console.log((-b)+Math.sqrt(-D)/2*a)
+//         console.log((-b)-Math.sqrt(-D)/2*a)
+//         console.log('Roots are imaginary')
+//     }
+//     else{
+//         ("equal roots")
+//     }
+ 
+//  else("imaginary roots")
+
+//first questions
+//  a=-9
+//  if(a<0){
+//     a*=1
+//     console.log("it is converted positive")
+// }else if(a>0){
+          
+//           console.log("it is positive")
+// }else{
+//     console.log("it is equal to zero")
 // }
+//third question
+
+// s1=12,
+// s2=0,
+// s3=13;
+// if(s1!=0 && s2!=0 && s3!=0){
+//     if(s1==s2||s2==s3||s3==s1){
+//         console.log("it is isoceles triangle")
+
+//     }
+//     else if(s1==s2&&s2==s3){
+//         console.log("it is equilateral triangle")
+//     }else{
+//         console.log("it is a scalene triangle")
+//     }
+// }else{
+//     console.log("it is not a triangle")
 // }
-// else{
-//     console.log(`${a} is less than ${b}`)
+
+
+//second question
+// i=30
+// c=100
+// t=i*c
+// if(t>1000){
+//     console.log(d=t*10/100)
+
+//     console.log(t-d)
 // }
 
 
-// var a="13"
-// if(+a){
-//     console.log("hey")
+//calculator
+// a=2,b=6
+// switch(8){
+// case 1:c=a+b
+// console.log(c)
+// break;
+// case 2:c=a-b
+// console.log(c)
+// break;
+// case 3:c=a*b
+// console.log(c)
+// break;
+// case 4:c=a/b
+// console.log(c)
+// break;
+// case 5:a**b
+// console.log(c)
+// break;
+// case 6:c=Math.floor(a)
+// console.log(c)
+// break;
+// case 7:c=Math.ceil(b)
+// console.log(c)
+// break;
+// case 8:c=Math.sqrt(a)
+// console.log(c)
+// break;
 // }
 
 
-a=15
-if((a%3)==0){
-    console.log("fizz ")
-}else if ((a%5)==0){
-console.log("buzz")
-}
- if(((a%3)==0 || (a%5)==0)){
-//     console.log("fizz")
-}
-else{
-    console.log("not a fizz buzz")
- }
+//calculator using conditions
+// a=1,b=3;
+// oper='Math.ceil'
+// if(oper=='+'){
+// c=a+b
+// console.log(c)
+// }else if(oper=='-'){
+// c=a-b
+// console.log(c)
+// }else if(oper=='*'){
+// c=a*b
+// console.log(c)
+// }else if(oper=='/'){
+//     c=a/b
+//     console.log(c)
+// }else if(oper=='%'){
+// c=a%b
+// console.log(c)
+// }else if(oper=="**"){
+//     c=a**b
+//     console.log(c)
+// }else if(oper=="Math.sqrt"){
+//     c1=Math.sqrt(a)
+//     c2=Math.sqrt(b)
+//     console.log(c1)
+//     console.log(c2)
+
+// }else if(oper=="Math.floor"){
+//     c1=Math.floor(b)
+//     c2=Math.floor(a)
+//     console.log(c1)
+//     console.log(c2)
+
+// }else if(oper=="Math.ceil"){
+//     c1=Math.ceil(a)
+//     c2=Math.ceil(b)
+//     console.log(c1)
+//     console.log(c2)
+    
+// }
